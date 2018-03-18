49a4158f493a1b3469f7bf0ae170c1043c59da44
Jacob Borella <jacobborella@hotmail.com>
2018-03-18 18:01:49 +0100

    nu virker pipeline også første gang + nexus tilføjet

--- ab-deploy.sh
+++ ab-deploy.sh
@@ -6,4 +6,3 @@ oc new-project jbl-dev
 oc policy add-role-to-user edit system:serviceaccount:ab-pipeline-demo:jenkins
 oc new-project jbl-prod
 oc policy add-role-to-user edit system:serviceaccount:ab-pipeline-demo:jenkins
-#der mangler oc create route her og også at lave den initielle app. Endnu bedre put det i pipeline, så den identificerer at første gang er anderledes
